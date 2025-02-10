b395b8826ac2a7908c0c46e5a7d6003209833ae7
mamatereza69 <karpov.it.04@gmail.com>
2025-02-10 20:42:05 +0500

    ignore db

--- db_handlers/create.py
+++ db_handlers/create.py
@@ -6,14 +6,27 @@ def create_db():
     cursor = conn.cursor()
 
     # Создаем таблицу для хранения напоминаний (если таблица не существует)
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS reminders (
-            user_id INTEGER PRIMARY KEY,
-            reminder_time TEXT,
-            timezone TEXT
+    cursor.execute('''           
+        CREATE TABLE IF NOT EXISTS timezones (
+            timezone_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT,
+            offset_utc TEXT
         )
     ''')
+
+    cursor.execute('''           
+        CREATE TABLE IF NOT EXISTS reminders (
+            user_id INTEGER PRIMARY KEY,
+            reminder_time_first TEXT,
+            reminder_time_second TEXT,
+            timezone_id INTEGER, 
+            FOREIGN KEY (timezone_id) REFERENCES timezones(timezone_id)
+        )
+    ''')
+
     conn.commit()
     conn.close()
+
+def conn
 
 create_db()
